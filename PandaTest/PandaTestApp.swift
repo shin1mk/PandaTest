e6f8543b975268deb778e18f107c69635a7a0256
SHIN MIKHAIL <shin.m.dp@gmail.com>
2024-02-15 09:45:47 +0200

    Initial Commit

---
+++ PandaTest/PandaTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PandaTestApp.swift
+//  PandaTest
+//
+//  Created by SHIN MIKHAIL on 15.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct PandaTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
